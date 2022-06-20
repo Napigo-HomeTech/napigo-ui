c7752a475abcaccb8ce88b9a61d7c3ce87253e56
Napihup <programminghup@gmail.com>
2022-06-20 15:51:44 +0800

    update Button Component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,6 +1,7 @@
-import React, { useMemo } from 'react';
+import React, { ReactElement, useMemo } from 'react';
 
 const ButtonColorClassMap = {
+  base: 'border-tranparent hover:bg-primary',
   primary: 'btn-primary text-primary-content',
   secondary: 'btn-secondary text-secondary-content',
   accent: 'btn-accent text-accent-content',
@@ -8,6 +9,14 @@ const ButtonColorClassMap = {
   warning: 'btn-warning text-warning-content',
   success: 'btn-success text-success-content',
   error: 'btn-error text-error-content',
+  ghost: 'btn-ghost',
+  'ghost-primary': 'btn-ghost text-primary active:border-primary',
+  'ghost-secondary': 'btn-ghost text-secondary active:border-secondary',
+  'ghost-accent': 'btn-ghost text-accent active:border-accent',
+  'ghost-success': 'btn-ghost text-success active:border-success',
+  'ghost-info': 'btn-ghost text-info active:border-info',
+  'ghost-warning': 'btn-ghost text-warning active:border-warning',
+  'ghost-error': 'btn-ghost text-error active:border-error',
 };
 const ButtonSizeClassMap = {
   sm: 'btn-sm',
@@ -15,20 +24,34 @@ const ButtonSizeClassMap = {
   lg: 'btn-lg',
 };
 type Sizing = 'sm' | 'md' | 'lg';
+type IconPosition = 'left' | 'right';
+
 type Color =
+  | 'base'
   | 'primary'
   | 'accent'
   | 'secondary'
   | 'info'
   | 'success'
   | 'warning'
-  | 'error';
+  | 'error'
+  | 'ghost'
+  | 'ghost-primary'
+  | 'ghost-secondary'
+  | 'ghost-accent'
+  | 'ghost-success'
+  | 'ghost-info'
+  | 'ghost-warning'
+  | 'ghost-error';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text?: string;
   size?: Sizing;
   color?: Color;
   outline?: boolean;
+  renderIcon?: () => JSX.Element | ReactElement;
+  iconPosition?: IconPosition;
+  square?: boolean;
 }
 
 export const Button: React.FC<ButtonProps> = (props) => {
@@ -37,19 +60,26 @@ export const Button: React.FC<ButtonProps> = (props) => {
     size = 'md',
     color = 'primary',
     outline = false,
+    renderIcon = () => {},
+    iconPosition = 'left',
+    square = false,
     className,
     ...buttonProps
   } = props;
 
   const classes = useMemo(() => {
-    return `btn ${outline ? 'btn-outline' : ''} ${ButtonSizeClassMap[size]} ${
-      ButtonColorClassMap[color]
-    }`;
+    return `btn ${square ? 'btn-square' : ''} gap-2 ${
+      outline ? 'btn-outline' : ''
+    } ${ButtonSizeClassMap[size]} ${ButtonColorClassMap[color]}`;
   }, [size, color, outline]);
 
   return (
     <button {...buttonProps} className={classes}>
-      {text}
+      <>
+        {iconPosition === 'left' && renderIcon()}
+        {text}
+        {iconPosition === 'right' && renderIcon()}
+      </>
     </button>
   );
 };
--- stories/Buttons/Button.stories.tsx
+++ stories/Buttons/Button.stories.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import { Button } from '../../src/components/button/Button';
+import { FaBeer } from 'react-icons/fa';
 
 const meta: Meta = {
-  title: 'Elements/Button',
+  title: 'Elements/Buttons',
   component: Button,
 };
 
@@ -12,9 +13,37 @@ export default meta;
 const Template: Story = (args) => <Button {...args} />;
 
 /** */
-export const StandardButton = Template.bind({});
-StandardButton.args = {
+export const TextButton = Template.bind({});
+TextButton.args = {
   text: 'Button',
   size: 'md',
   color: 'primary',
+};
+
+export const ButtonWithIcon = Template.bind({});
+ButtonWithIcon.args = {
+  size: 'md',
+  text: 'Hello',
+  color: 'primary',
+  renderIcon: () => {
+    return <FaBeer />;
+  },
+};
+export const ButtonIcon = Template.bind({});
+ButtonIcon.args = {
+  size: 'md',
+  color: 'ghost',
+  renderIcon: () => {
+    return <FaBeer />;
+  },
+};
+
+export const ButtonIconOutline = Template.bind({});
+ButtonIconOutline.args = {
+  size: 'md',
+  color: 'ghost',
+  outline: true,
+  renderIcon: () => {
+    return <FaBeer />;
+  },
 };
